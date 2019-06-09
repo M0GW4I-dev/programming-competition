1345db6a7df8f5704fcf144f43e308224dcb8ee4
b1u3 <bp16071@shibaura-it.ac.jp>
2019-06-10 00:30:59 +0900

    いらないファイル消した

--- ARC2018/101ARC/sort.py
+++
@@ -1,8 +0,0 @@
-def main():
-    n = int(input())
-    a = [int(i) for i in input().split()]
-    a.sort()
-    print(a)
-
-if __name__ == '__main__':
-    main()
--- ARC2018/95ARC/fact.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-
-
-def main():
-    t1 = time.time()
-    n = int(sys.argv[1])
-
-
